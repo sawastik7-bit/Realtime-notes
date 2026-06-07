49e6e5970b443b2ab1818daa302da1e2bd44af1e
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-07 22:53:36 +0530

    configured the readme and added the details

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,21 @@
 import express from 'express';
-
-import http from 'http';
-import {Server} from 'socket.io';
-
+import roomsRouter from './src/routes/roomsRoutes/roomsRoutes.js';
 
 const app=express();
-const server=http.createServer(app);
+app.use(express.json());
 
-const io=new Server(server);
-
-
-io.on("connection",(socket)=>{
-    console.log("a user is connected");
-
-
-   socket.on("joinRoom",(room)=>{
-    console.log(` socket ${socket.id} joining room`,room);
-    socket.join(room);
-   })
-
-   socket.on("roomMessage",(data)=>{
-    console.log("message from the client is ", data);
-
-    io.to("room1").emit("roomMessage",`message from teh server to ${socket.id}`);
-
-   });
+app.use("/api/rooms",roomsRouter);
 
 
 
-   
-    socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-    
-});
- 
 
-app.get('/',(req,res)=>{
-    res.send("this is the server ");
-})
 
-server.listen(5000,()=>{
+app.listen(5000,()=>{
     console.log("server is running on port 5000");
 })
+
+
+
+
+
+
